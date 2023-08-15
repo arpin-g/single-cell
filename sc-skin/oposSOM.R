486da942be1570af3861e2eee79ddcfee2485563
Arpine Grigoryan <111293540+arpin-g@users.noreply.github.com>
2023-08-15 13:35:53 +0400

    Update oposSOM.R

--- sc-skin/oposSOM.R
+++ sc-skin/oposSOM.R
@@ -2,10 +2,11 @@
 # install_github("hloefflerwirth/oposSOM")
 
 library(oposSOM)
-sm <- readRDS("rds/fic_metacells.rds")
+
+sm <- fic_metacells
 
 # seurat <- sm[[1]]
-# metacell.data <- sm[[2]]
+metacell.data <- sm[[2]]
 
 env <- opossom.new(list(dataset.name = "2023_08_Reynolds_fic_som_1",
                         database.host = "https://apr2022.archive.ensembl.org/",
@@ -15,14 +16,15 @@ env <- opossom.new(list(dataset.name = "2023_08_Reynolds_fic_som_1",
                         spot.threshold.modules = 0.92 ) )
 
 # Load input data
-env$indata <- sm[[2]]
+env$indata <- metacell.data
 
-# cln1 <- sub("_c.*", "", colnames(env$indata))
-# cln2 <- ifelse(grepl("Pericyte|Schwann|KC",cln1),  gsub("_", "", cln1), cln1)
-
+extract_part <- function(value) {
+  parts <- unlist(strsplit(value, "_adult|_fetal"))
+  return(parts[1])
+}
 
 # Define sample groups
-env$group.labels <- paste0(XXX)
+env$group.labels <- sapply(colnames(env$indata), extract_part)
 o <- order( env$group.labels)
 env$group.labels <- env$group.labels[o]
 env$indata <- env$indata[,o]
